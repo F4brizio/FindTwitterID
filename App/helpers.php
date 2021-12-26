e56ac24b450036bebf1153ad11a41b21516df869
Fabrizio <68043260+F4brizio@users.noreply.github.com>
2021-12-25 23:52:25 -0500

    add new helpers
    - toJson
    - env()

--- App/helpers.php
+++ App/helpers.php
@@ -13,3 +13,14 @@ if(!function_exists('cache')){
         {if (!self::$instance) { self::$instance = new Psr16Adapter('Files'); } return self::$instance;}
     }
 }
+if(!function_exists('toJson')){
+    function toJson($data){
+        echo json_encode($data);
+        return null;
+    }
+}
+if(!function_exists('env')){
+    function env($key, $default = null){
+        return (isset($_ENV[$key])) ? $_ENV[$key] : $default;
+    }
+}

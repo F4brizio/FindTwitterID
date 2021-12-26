3586eeea43dc2da47c85094bbad80025f6e00d73
Fabrizio <68043260+F4brizio@users.noreply.github.com>
2021-12-25 23:52:49 -0500

    update folder cache

--- index.php
+++ index.php
@@ -4,16 +4,14 @@ use BulveyzRouter\Route;
 use BulveyzRouter\Router;
 use Phpfastcache\CacheManager;
 use Phpfastcache\Config\ConfigurationOption;
-use App\Controllers\HomeController;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 date_default_timezone_set("America/Lima");
 
-CacheManager::setDefaultConfig(new ConfigurationOption([
-    'path' => __DIR__.'/storage/xcache',
-]));
+CacheManager::setDefaultConfig(new ConfigurationOption(['path' => __DIR__.'/Storage/cache']));
+
 Route::get('/', 'Controllers\HomeController@index');
 Route::post('/get', 'Controllers\HomeController@get');
 Router::routeVoid();
